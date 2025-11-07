51ab6b83da95067db1a590d661f323b390068997
rill <e1303025078@oit.ac.jp>
2025-11-07 12:35:06 +0900

    2025/11/07/12:34 藤田 余分なものを消去

--- OneButtonGame_b/Assets/Scripts/GameManager.cs
+++ OneButtonGame_b/Assets/Scripts/GameManager.cs
@@ -18,7 +18,6 @@ public class GameManager : MonoBehaviour
     [Header("UIコンポーネント")]
     public GameObject timerPanel;
     public TextMeshProUGUI timerText;
-    public GameObject countdownPanel;
     public TextMeshProUGUI countdownText;
     public GameObject playerHealthPanel;
     public GameObject BossHpPanel;
@@ -95,18 +94,11 @@ public class GameManager : MonoBehaviour
     void Start()
     {
          timerPanel = GameObject.Find("TimerPanel");
-         timerText = timerPanel.GetComponent<TextMeshProUGUI>();
-         countdownPanel = GameObject.Find("CountDown");
-         countdownText = countdownPanel.GetComponent<TextMeshProUGUI>();
          playerHealthPanel = GameObject.Find("PlayerHearts");
          BossHpPanel = GameObject.Find("BossHP");
          coreStatusPanel = GameObject.Find("CoreStatus");
-         coreStatusText = coreStatusPanel.GetComponent<TextMeshProUGUI>();
-         //fpsText = GameObject.Find("FPS");
          bossExplanationPanel = GameObject.Find("");
-         bossExplanationText = bossExplanationPanel.GetComponent<TextMeshProUGUI>();
          coreExplanationPanel = GameObject.Find("");
-         coreExplanationText = coreExplanationPanel.GetComponent<TextMeshProUGUI>();
     }
 
 
